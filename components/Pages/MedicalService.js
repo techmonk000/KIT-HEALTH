952437ffb60f4ee7fe777622324483db16195a98
Supriyo Saha <mm.saha1982@gmail.com>
2023-02-25 01:27:11 +0530

    Create MedicalService.js

---
+++ components/Pages/MedicalService.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+import ServiceForm from "../components/ServiceForm";
+
+const MedicalService = () =>{
+    return(
+        <div>
+          <Header />
+          <ServiceForm/>
+        </div>
+    )
+};
+export default MedicalService;
+
+
