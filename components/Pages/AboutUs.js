7f82f8004842497cd1f009067025324286f3a007
Supriyo Saha <mm.saha1982@gmail.com>
2023-02-25 01:24:01 +0530

    Update AboutUs.js

--- components/Pages/AboutUs.js
+++ components/Pages/AboutUs.js
@@ -1,1 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
 
+import AboutContent from "../components/AboutContent";
+
+const About = () =>{
+    return(
+        <div>
+          <Header />
+          <AboutContent />
+          
+        </div>
+    )
+};
+export default About;
