804b5996c756fabd4498edba3612d7a581300a17
Supriyo Saha <mm.saha1982@gmail.com>
2023-02-25 00:48:47 +0530

    Update Life.js

--- components/Confirmation Pages/Life.js
+++ components/Confirmation Pages/Life.js
@@ -1,1 +1,12 @@
+import React from "react";
+import Header from "../Header";
 
+const Life = () =>{
+    return(
+        <div>
+          <Header />
+          
+        </div>
+    )
+};
+export default Life;
