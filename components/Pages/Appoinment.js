d0cc551c7417cd59fa25f90fd826c552278067ff
Supriyo Saha <mm.saha1982@gmail.com>
2023-02-25 01:24:31 +0530

    Create Appoinment.js

---
+++ components/Pages/Appoinment.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+import Appoinmentform from "../components/AppoinmentForm";
+const Appoinment = () =>{
+    return(
+        <div>
+          <Header />
+          <Appoinmentform/>
+        </div>
+    )
+};
+export default Appoinment;
