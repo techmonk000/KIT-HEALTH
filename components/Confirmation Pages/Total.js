5fe81f11953cce264a050ef3f1d48c86e4e4f1c4
Supriyo Saha <mm.saha1982@gmail.com>
2023-02-25 00:49:06 +0530

    Update Total.js

--- components/Confirmation Pages/Total.js
+++ components/Confirmation Pages/Total.js
@@ -1,1 +1,12 @@
+import React from "react";
+import Header from "../Header";
 
+const Total = () =>{
+    return(
+        <div>
+          <Header />
+          
+        </div>
+    )
+};
+export default Total;
