90f7ee71214fb501d71f89c8986c267309f70cb6
Supriyo Saha <mm.saha1982@gmail.com>
2023-02-25 01:27:32 +0530

    Create Order.js

---
+++ components/Pages/Order.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Header from "../components/Header";
+import Entry from "../components/EntryMedicines";
+import medicines from "../medicines";
+
+function createEntry(medicineTerm) {
+  return (
+    <Entry
+      key={medicineTerm.id}
+      medicine={medicineTerm.medicine}
+      name={medicineTerm.name}
+      description={medicineTerm.properties}
+    />
+  );
+}
+
+
+const Order = () =>{
+    return(
+        <div>
+          <Header />
+          <h1>
+        <span>Order Medicines</span>
+      </h1>
+      <dl className="dictionary">{medicines.map(createEntry)}</dl>
+        </div>
+    )
+};
+export default Order;
