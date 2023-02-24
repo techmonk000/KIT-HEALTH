bdd82d764fba1bea4d1150159c0ca7ee27ad5bdb
Supriyo Saha <mm.saha1982@gmail.com>
2023-02-25 00:46:59 +0530

    Update InsurancePricing.js

--- components/InsurancePricing.js
+++ components/InsurancePricing.js
@@ -1,1 +1,191 @@
+import React from "react";
 
+
+const InsurancePricing = () =>{
+    return(
+        <div>
+          <div id="generic_price_table">   
+<section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    
+                    <div class="price-heading clearfix">
+                        <h1>Protect Your Family With KIT-HEALTH</h1>
+                    </div>
+                  
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            
+           
+            <div class="row">
+                <div class="col-md-4">
+                
+                 
+                    <div class="generic_content clearfix">
+                        
+                        
+                        <div class="generic_head_price clearfix">
+                        
+                           
+                            <div class="generic_head_content clearfix">
+                            
+                           
+                                <div class="head_bg"></div>
+                                <div class="head">
+                                    <span>Health Insurance</span>
+                                </div>
+                               
+                                
+                            </div>
+                           
+                            
+                          
+                            <div class="generic_price_tag clearfix">  
+                                <span class="price">
+                                    <span class="sign">Rs.</span>
+                                    <span class="currency">99</span>
+                                    <span class="cent">.99</span>
+                                    <span class="month">/MON</span>
+                                </span>
+                            </div>
+                           
+                            
+                        </div>                            
+                        
+                        <div class="generic_feature_list">
+                          <ul>
+                              <li><span>2000+</span> Hospitals</li>
+                                <li><span>Total</span> Cashless Facility</li>
+                                <li><span>Across</span> The World</li>
+                                <li><span>70+</span> Medical Tests</li>
+                                <li><span>Complete</span> Coverage</li>
+                            </ul>
+                        </div>
+                      
+                        <div class="generic_price_btn clearfix">
+                          <a class="health" href="/goHealth">Go Protect</a>
+                        </div>
+                       
+                        
+                    </div>
+                   
+                        
+                </div>
+                
+                <div class="col-md-4">
+                
+                
+                    <div class="generic_content active clearfix">
+                        
+                      
+                        <div class="generic_head_price clearfix">
+                        
+                          
+                            <div class="generic_head_content clearfix">
+                         
+                                <div class="head_bg"></div>
+                                <div class="head">
+                                    <span>Life Insurance</span>
+                                </div>
+                               
+                            </div>
+                          
+                            <div class="generic_price_tag clearfix">  
+                                <span class="price">
+                                    <span class="sign">Rs.</span>
+                                    <span class="currency">199</span>
+                                    <span class="cent">.99</span>
+                                    <span class="month">/MON</span>
+                                </span>
+                            </div>
+                            
+                            
+                        </div>                            
+                       
+                        <div class="generic_feature_list">
+                          <ul>
+                              <li><span>Easy</span> Claims</li>
+                                <li><span>00%</span> Returns</li>
+                                <li><span>Zero</span> Processing Charge</li>
+                                <li><span>Multiple</span> Nominies</li>
+                                <li><span>Complete</span> Safety</li>
+                            </ul>
+                        </div>
+                     
+                        <div class="generic_price_btn clearfix">
+                          <a class="life" href="/goLife">Go Protect</a>
+                        </div>
+                        
+                        
+                    </div>
+                    
+                        
+                </div>
+                <div class="col-md-4">
+                
+                 
+                    <div class="generic_content clearfix">
+                        
+                      
+                        <div class="generic_head_price clearfix">
+                        
+                            
+                            <div class="generic_head_content clearfix">
+                            
+                              
+                                <div class="head_bg"></div>
+                                <div class="head">
+                                    <span>Health + Life</span>
+                                </div>
+                                
+                                
+                            </div>
+                           
+                            <div class="generic_price_tag clearfix">  
+                                <span class="price">
+                                    <span class="sign">Rs.</span>
+                                    <span class="currency">299</span>
+                                    <span class="cent">.99</span>
+                                    <span class="month">/MON</span>
+                                </span>
+                            </div>
+                            
+                            
+                        </div>                            
+                        
+                        <div class="generic_feature_list">
+                          <ul>
+                              <li><span>3000+</span> Hospitals</li>
+                                <li><span>Complete</span> Cashless</li>
+                                <li><span>Zero</span> Processing fees</li>
+                                <li><span>00%</span> Returns</li>
+                                <li><span>Total</span> Coverave</li>
+
+                            </ul>
+                        </div>
+                       
+                        
+                        
+                        <div class="generic_price_btn clearfix">
+                          <a class="totalCoverage" href="/totalCoverage">Go Protect</a>
+                        </div>
+                        
+                        
+                    </div>
+                  
+                        
+                </div>
+            </div>  
+            
+        
+            
+        </div>
+    </section>
+        </div>
+        </div>
+    )
+};
+export default InsurancePricing;
