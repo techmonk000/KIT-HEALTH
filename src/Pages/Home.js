96e67011d59adedd060f143552f8ff3212f32c28
Mrinmay <mrinmaydas2604@gmail.com>
2023-02-25 10:57:47 +0530

    logo updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,6 +5,7 @@ const Home = () =>{
     return(
         <div>
           <Header />
+          <iframe src="https://i.pinimg.com/originals/b8/f6/bf/b8f6bf5e5b054f2487d735a0c324a01f.jpg" width="320" height="320" align="left"></iframe>
          
         </div>
     )
